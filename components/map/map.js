740a765eb98d63230355c398fdc140a2d39d8d12
Dylan Drake <ddrake@geo-jobe.com>
2026-05-21 00:46:31 -0500

    map, featurelayer, temporary Vite proxy for server.js before we migrate the persistance layer to ArcGIS

---
+++ components/map/map.js
@@ -0,0 +1,29 @@
+import MapView from "@arcgis/core/views/MapView.js";
+import WebMap from "@arcgis/core/WebMap.js";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer.js";
+
+export async function createMap(featureLayerUrl) {
+  const container = document.createElement("div");
+  container.id = "map-container";
+
+  const pointsLayer = new FeatureLayer({
+    url: featureLayerUrl,
+  });
+  await pointsLayer.load();
+
+  const webMap = new WebMap({
+    basemap: "topo-vector",
+    layers: [pointsLayer],
+  });
+
+  const view = new MapView({
+    container,
+    map: webMap,
+  });
+
+  view.when(() => {
+    view.goTo(pointsLayer.fullExtent).catch(() => {});
+  });
+
+  return { container, pointsLayer };
+}
